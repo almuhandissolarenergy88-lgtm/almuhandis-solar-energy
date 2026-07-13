6d6a7a0c9398adc2e3f6e15f78f8bef1ffa5e635
ahmad kh <301775996+almuhandissolarenergy88-lgtm@users.noreply.github.com>
2026-07-14 00:39:01 +0300

    Add main menu links in menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,6 @@
-
+const menu = document.getElementById('mainMenu');
+menu.innerHTML = `
+    <a href="#" style="color:white; text-decoration:none;">الرئيسية</a>
+    <a href="#" style="color:white; text-decoration:none;">خدماتنا</a>
+    <a href="#" style="color:white; text-decoration:none;">اتصل بنا</a>
+`;

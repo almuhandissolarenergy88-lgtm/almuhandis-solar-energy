dfc1fd423aa873ec5efc6134f084d4efa4eeecd5
ahmad kh <301775996+almuhandissolarenergy88-lgtm@users.noreply.github.com>
2026-07-14 00:30:38 +0300

    Refactor services and add main menu items
    Removed services array and its rendering logic. Added menu items to the main menu.

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-const services = [
-    { icon: "fa-solar-panel", name: "ألواح شمسية" },
-    { icon: "fa-bolt", name: "إنفرترات" },
-    { icon: "fa-water", name: "طلمبات مياه" },
-    { icon: "fa-battery-full", name: "بطاريات" }
-];
-
-const container = document.getElementById('services');
-services.forEach(item => {
-    container.innerHTML += `
-        <div class="card">
-            <i class="fas ${item.icon} fa-2x" style="color:#25d366"></i>
-            <p>${item.name}</p>
-        </div>
-    `;
-});
+const menu = document.getElementById('mainMenu');
+menu.innerHTML = `
+    <a href="#" style="color:white; text-decoration:none;">الرئيسية</a>
+    <a href="#" style="color:white; text-decoration:none;">خدماتنا</a>
+    <a href="#" style="color:white; text-decoration:none;">اتصل بنا</a>
+`;

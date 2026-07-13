03bca70c94de4a9c1f7fd98851aeba0f783b7f11
ahmad kh <301775996+almuhandissolarenergy88-lgtm@users.noreply.github.com>
2026-07-14 00:33:39 +0300

    Refactor menu and add services data rendering

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const menu = document.getElementById('mainMenu');
-menu.innerHTML = `
-    <a href="#" style="color:white; text-decoration:none;">الرئيسية</a>
-    <a href="#" style="color:white; text-decoration:none;">خدماتنا</a>
-    <a href="#" style="color:white; text-decoration:none;">اتصل بنا</a>
-`;
+const data = [
+    { icon: "fa-solar-panel", title: "ألواح شمسية" },
+    { icon: "fa-bolt", title: "إنفرترات" },
+    { icon: "fa-water", title: "طلمبات" },
+    { icon: "fa-battery-full", title: "بطاريات" },
+    { icon: "fa-tools", title: "صيانة" },
+    { icon: "fa-headset", title: "دعم فني" }
+];
+
+const container = document.getElementById('services');
+data.forEach(item => {
+    container.innerHTML += `
+        <div class="card">
+            <i class="fas ${item.icon} fa-2x" style="color:#25d366"></i>
+            <h3>${item.title}</h3>
+        </div>
+    `;
+});

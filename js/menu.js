bab4559e5d80651278a4509235d287e00e161920
ahmad kh <301775996+almuhandissolarenergy88-lgtm@users.noreply.github.com>
2026-07-14 01:09:06 +0300

    Update menu links and styles in menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById('mainMenu');
 menu.innerHTML = `
-    <a href="#" style="color:white; text-decoration:none;">الرئيسية</a>
-    <a href="#" style="color:white; text-decoration:none;">خدماتنا</a>
-    <a href="#" style="color:white; text-decoration:none;">اتصل بنا</a>
+    <a href="index.html" style="color:white; text-decoration:none; margin:0 15px;">الرئيسية</a>
+    <a href="projects.html" style="color:white; text-decoration:none; margin:0 15px;">مشاريعنا</a>
+    <a href="https://wa.me/201029394526" style="color:white; text-decoration:none; margin:0 15px;">اتصل بنا</a>
 `;

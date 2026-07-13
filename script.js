415d87c817cfa949458026cbd5dc22e08d21a1ce
ahmad kh <301775996+almuhandissolarenergy88-lgtm@users.noreply.github.com>
2026-07-14 00:18:09 +0300

    Add service cards to the services container

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("الموقع يعمل بنجاح");
+const services = [
+    { icon: "fa-solar-panel", name: "ألواح شمسية" },
+    { icon: "fa-bolt", name: "إنفرترات" },
+    { icon: "fa-water", name: "طلمبات مياه" },
+    { icon: "fa-battery-full", name: "بطاريات" }
+];
+
+const container = document.getElementById('services');
+services.forEach(item => {
+    container.innerHTML += `
+        <div class="card">
+            <i class="fas ${item.icon} fa-2x" style="color:#25d366"></i>
+            <p>${item.name}</p>
+        </div>
+    `;
+});
